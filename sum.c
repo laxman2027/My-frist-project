63ec75b6bf083ade93fbf9019e441575b376bf81
laxman2027 <mallaxman9@gmail.com>
2025-02-17 11:53:33 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<math.h>
+
+int t,r,a;
+
+int main()
+{
+    printf ("Enter The Amount :   \n");
+    scanf("%d",&a);
+
+    printf ("Enter The holding time :   \n");
+    scanf("%d",&t);
+
+    printf ("Enter The rate of interaste :   \n");
+    scanf("%d",&r);
+
+   int m = (r*t*a)/100 ;
+    int tax;
+
+printf("                       return :%d\n",m);
+
+if (m<=600000)
+   {
+    printf ("                        Tax : 0%% \n ");
+    printf("                   tax amount :%d\n",m*0,&tax);
+    printf("Total ammount You will Recive :%d\n",m+a-tax);
+   }
+   else if (600000<m<=1200000)
+   {
+    printf ("                       Tax : 10%% \n");
+    printf("                   tax amount :%d \n",(m*10)/100,&tax);
+    printf("Total ammount You will Recive :%d\n",m+a-tax);
+   }
+   else if (1200000<m)
+   {
+    printf ("                        Tax : 15%%  \n");
+    printf("                   tax amount :%d\n",(m*15)/100,&tax);
+    printf("Total ammount You will Recive :%d\n",m+a-tax);
+   }
+
+   
+    return 0;
+    
+}
+
+
+ 
